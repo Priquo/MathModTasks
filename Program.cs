1b37850ef21a8519b3fbd21a7e6df66925990b74
Helium Inert <helium.inert@outlook.com>
2021-06-02 14:46:20 +0300

    ok

--- Program.cs
+++ Program.cs
@@ -14,21 +14,8 @@ namespace MathModTasks
             //p.MainSolution();
             //Stank st = new Stank("data1.csv");
             //st.MainSolution();
-            double[] result = new double[2];
-            double[,] table_result;
             Simplex S = new Simplex(DataWorkerCSV.StringListConverter(DataWorkerCSV.ReadCSV("sipm.csv")));
-            table_result = S.Calculate(result);
-            for (int i = 0; i < table_result.GetLength(0); i++)
-            {
-                for (int j = 0; j < table_result.GetLength(1); j++)
-                    Console.Write(table_result[i, j] + "\t");
-                Console.WriteLine();
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Решение:");
-            Console.WriteLine("X[1] = " + result[0]);
-            Console.WriteLine("X[2] = " + result[1]);
+            S.MakeResult();
             Console.ReadKey();
         }
     }
--- Simplex.cs
+++ Simplex.cs
@@ -43,7 +43,7 @@ namespace MathModTasks
         }
 
         //result - в этот массив будут записаны полученные значения X
-        public double[,] Calculate(double[] result)
+        double[,] Calculate(double[] result)
         {
             int mainCol, mainRow; //ведущие столбец и строка
 
@@ -81,7 +81,22 @@ namespace MathModTasks
 
             return table;
         }
-
+        public void MakeResult()
+        {
+            double[] result = new double[2];
+            double[,] table_result;
+            table_result = Calculate(result);
+            for (int i = 0; i < table_result.GetLength(0); i++)
+            {
+                for (int j = 0; j < table_result.GetLength(1); j++)
+                    Console.Write(table_result[i, j] + "\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Решение:");
+            Console.WriteLine("X[1] = " + result[0]);
+            Console.WriteLine("X[2] = " + result[1]);
+        }
         private bool IsItEnd()
         {
             bool flag = true;
